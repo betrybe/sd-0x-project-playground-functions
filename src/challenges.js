752ef5323373fac4f4ea3354fc828a3997b01e4b
Bruno Batista <brunobatista101@gmail.com>
2021-03-01 11:07:31 -0300

    nova versao do projeto

--- src/challenges.js
+++ src/challenges.js
@@ -46,26 +46,6 @@ function decode() {
   // seu código aqui
 }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -75,10 +55,6 @@ module.exports = {
   encode,
   fizzBuzz,
   footballPoints,
-  generatePhoneNumber,
-  techList,
   highestCount,
-  hydrate,
   splitSentence,
-  triangleCheck,
 };
